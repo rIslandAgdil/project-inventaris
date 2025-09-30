7bff182ce0b96df3665ad99cd394047fb9db5bc1
Networking BPVP Sorong <nofryanti.ainun19@gmail.com>
2025-09-30 14:19:43 +0900

    buka readme.md

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const barangRoutes = require('./routers/barangRouters');
+
+const app =  express();
+app.use(bodyParser.json());
+
+app.use('/api', barangRoutes);
+
+const PORT = 3001;
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
 
